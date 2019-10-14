c80441a6157d3a84d0f6d14d618648d81f39412d
Daniel Adeniji <daniel_adeniji@hotmail.com>
2019-10-14 08:09:34 -0700

    Changed from accent sensitivity to Accent In-sensitive

--- sqlnexus/SQLNexus_PreProcessing.sql
+++
@@ -1,8 +0,0 @@
-select 'Nexus preprocessing scripts running' [message]
-
-go
-
-if object_id ('tblNexusInfo') is null
-begin
-	create table dbo.tblNexusInfo (Attribute nvarchar (200) unique, Value nvarchar(2048))
-end
