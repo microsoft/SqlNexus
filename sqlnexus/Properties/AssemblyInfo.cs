cee4c8e50724360ab1b7905f3eb7b7eac6df350a
PiJoCoder <pijojnk@hotmail.com>
2020-10-01 15:24:07 -0500

    changing versioning scheme to major version + YY.MM.DD

--- sqlnexus/Properties/AssemblyInfo.cs
+++ sqlnexus/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.0.0.9")]
-[assembly: AssemblyFileVersion("6.0.0.9")]
+[assembly: AssemblyVersion("6.20.10.1")]
+[assembly: AssemblyFileVersion("6.20.10.1")]
