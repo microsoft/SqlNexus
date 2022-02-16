e598ab09d7f50d9a73e02e1f0f2058d545d7b65d
JosephPilov <23519517+PiJoCoder@users.noreply.github.com>
2022-02-16 16:49:45 -0600

    Version bump

--- sqlnexus/Properties/AssemblyInfo.cs
+++ sqlnexus/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.22.02.09")]
-[assembly: AssemblyFileVersion("7.22.02.09")]
+[assembly: AssemblyVersion("7.22.02.16")]
+[assembly: AssemblyFileVersion("7.22.02.16")]
