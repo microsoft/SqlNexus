e1a9ef4db9afca4e74212e3960ce13a8750b80fe
JosephPilov <23519517+PiJoCoder@users.noreply.github.com>
2022-02-20 21:01:08 -0600

    Fixing #133 sending all output to the main output file

--- sqlnexus/PostProcess.cmd
+++ sqlnexus/PostProcess.cmd
@@ -4,8 +4,7 @@ set SQLServer=%1
 set Database=%2
 set ImportPath=%3
 
-@echo %date% %time% parameters: %SQLServer% %Database% %ImportPath% > %temp%\sqlnexus_postprocessing.out
-
+@echo %date% %time% parameters: %SQLServer% %Database% %ImportPath%
 
 
 setlocal
@@ -16,28 +15,27 @@ if "%servername%"=="" goto usage
 if "%filepath%"=="" goto usage
 
 
-@echo %date% %time% SQL Nexus PostProcessing >> %temp%\sqlnexus_postprocessing.out
+@echo %date% %time% SQL Nexus PostProcessing 
 
 @echo %date% %time% Creating tblPlansTemp >> %temp%\sqlnexus_postprocessing.out
 sqlcmd.exe -S%SQLServer% -E -d%Database% -Q"create table tblPlansTemp (sqlplan xml)"
 
-
 @echo %date% %time% Inserting into tblPlansTemp >> %temp%\sqlnexus_postprocessing.out
 for /f %%j in ('dir /b "%ImportPath%\*.sqlplan"') do sqlcmd.exe -S%SQLServer%  -E -d%Database% -Q"insert into tblPlansTemp select  cast(cast (BulkColumn as varchar(max)) as xml) sqlplan from openrowset (bulk '%filepath%\%%j',single_blob) as doc"
 
-@echo %date% %time% Calling SQLNexus_PostProcessing.sql >> %temp%\sqlnexus_postprocessing.out
-sqlcmd.exe -S%SQLServer% -E -d%Database% -iSQLNexus_PostProcessing.sql >> %temp%\sqlnexus_postprocessing.out
-
+@echo %date% %time% Calling SQLNexus_PostProcessing.sql 
+sqlcmd.exe -S%SQLServer% -E -d%Database% -iSQLNexus_PostProcessing.sql 
 
 rem sqlcmd.exe -S%SQLServer% -E -otoptables.out -Q"set QUOTED_IDENTIFIER on; WITH XMLNAMESPACES ('http://schemas.microsoft.com/sqlserver/2004/07/showplan' AS sp)  select distinct  stmt.stmt_details.value ('@Database', 'varchar(max)') 'Database' ,  stmt.stmt_details.value ('@Schema', 'varchar(max)') 'Schema' ,   stmt.stmt_details.value ('@Table', 'varchar(max)') 'table'  from (  select sqlplan from tempdb.dbo.tblPlansTemp) as p       cross apply sqlplan.nodes('//sp:Object') as stmt (stmt_details) "
 
 
-@echo %date% %time% SQL Nexus PostProcessing complete >> %temp%\sqlnexus_postprocessing.out
+@echo %date% %time% SQL Nexus PostProcessing complete
 
 goto end
 
 :usage
-echo Usage:  %~n0 servername filepath
+@echo Incorrect syntaxt. Proper usage:  %~n0 servername filepath
+
 
 :end
 
