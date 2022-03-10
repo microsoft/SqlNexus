microsoft/SqlNexus

commit fb82b7acbe61778e99ea89ef2a7ef73ef43a147b

Adding an extra ELSE IF check. This is to cover the eventuality that someone called ValidateData() with the source and destination types being the same. Seems like this is a feature/limitation of CLR

RowsetImportEngine/Helpers/ConvertHelper.cs
@@ -15,6 +15,10 @@ namespace RowsetImportEngine.Helpers
                 {
                     data = (T)converter.ConvertFrom(columndata);
                 }
+                else if (converter != null && converter.IsValid(columndata.ToString()))
+                {
+                    data = (T)converter.ConvertFrom(columndata.ToString());
+                }
 
                 return data;
             }

commit e17dbf3c02b4c325f3c938a4439b067a15f63ecd

Fixing an Null exception that was introduced by fb82b7acbe61778e99ea89ef2a7ef73ef43a147b

RowsetImportEngine/Helpers/ConvertHelper.cs
@@ -18,9 +18,13 @@ namespace RowsetImportEngine.Helpers
                 {
                     data = (T)converter.ConvertFrom(columndata);
                 }
-                else if (converter != null && converter.IsValid(columndata.ToString()))
+                else if (converter != null && columndata != null)
                 {
-                    data = (T)converter.ConvertFrom(columndata.ToString());
+                    if (converter.IsValid(columndata.ToString()))
+                    {
+                        data = (T)converter.ConvertFrom(columndata.ToString());
+                    }
+                    
                 }
 
                 return data;