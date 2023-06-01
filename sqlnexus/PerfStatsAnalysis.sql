dff1715c39b832aa19176d78561d36443b7c6928
JosephPilov-msft <23519517+PiJoCoder@users.noreply.github.com>
2023-06-01 07:20:17 -0500

    Fixing the Xevent rule to point to the right file #229
    Co-Authored-By: asavioli <49494532+asavioli@users.noreply.github.com>

--- sqlnexus/PerfStatsAnalysis.sql
+++ sqlnexus/PerfStatsAnalysis.sql
@@ -3666,7 +3666,7 @@ IF (OBJECT_ID ('tbl_XEvents') IS NOT NULL)
 	BEGIN 
 		UPDATE tbl_AnalysisSummary
 		SET [Status] = 1,
-			Description = 'Expensive Xevents are active on the system. These can negatively impact performance. Examples include: '+ @events_string  + CHAR(13) + CHAR(10) + ' Consider disabling these and review the feasibility of using them long term. See *_MiscPssdiagInfo_Startup.OUT for details.',
+			Description = 'Expensive Xevents are active on the system. These can negatively impact performance. Examples include: '+ @events_string  + CHAR(13) + CHAR(10) + ' Consider disabling these and review the feasibility of using them long term. See *_ExistingProfilerXeventTraces.out  for details.',
 			Report = 'Active Traces'
 		WHERE Name = 'usp_Expensive_XEvts_Used'
 					
