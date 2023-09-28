3304546e05fb7ff50934fd2b16beff9d16151d63
JosephPilov-msft <23519517+PiJoCoder@users.noreply.github.com>
2023-09-28 12:21:12 -0500

    version bump

--- sqlnexus/Properties/AssemblyInfo.cs
+++ sqlnexus/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.23.06.06")]
-[assembly: AssemblyFileVersion("7.23.06.06")]
+[assembly: AssemblyVersion("7.23.09.13")]
+[assembly: AssemblyFileVersion("7.23.09.13")]
